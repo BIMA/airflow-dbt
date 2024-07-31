8e7b7bee3f43227beb497e869822de49c591a74f
bima <bimantara.hanumpraja@gmail.com>
2024-07-31 19:53:49 +0700

    update code

---
+++ airflow-manifest/dags/dag_fazz_agen.py
@@ -0,0 +1,64 @@
+
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.latest_only import LatestOnlyOperator
+from airflow.operators.sensors.external_task_sensor import ExternalTaskSensor
+from airflow.utils.dates import days_ago
+from airflow.operators.python import PythonOperator
+from datetime import timedelta
+
+default_args = {
+    'owner': 'FazzAgen_team',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+def run_dbt_model(model_name):
+    # Add your logic to run dbt model
+    print(f"Running dbt model: {model_name}")
+
+with DAG(
+    'dag_fazz_agen',
+    default_args=default_args,
+    description='This is a FA pipeline',
+    schedule_interval='None',
+    start_date=days_ago(1),
+    tags=['fazz-agen'],
+) as dag:
+
+    start = DummyOperator(task_id='start')
+
+    
+    fact_sta_topup = PythonOperator(
+        task_id='fact_sta_topup',
+        python_callable=run_dbt_model,
+        op_args=['fact_sta_topup'],
+    )
+    
+    fact_transactions = PythonOperator(
+        task_id='fact_transactions',
+        python_callable=run_dbt_model,
+        op_args=['fact_transactions'],
+    )
+    
+    fact_sta_withdrawals = PythonOperator(
+        task_id='fact_sta_withdrawals',
+        python_callable=run_dbt_model,
+        op_args=['fact_sta_withdrawals'],
+    )
+    
+
+    
+    
+    
+    
+    
+    
+    
+
+    end = DummyOperator(task_id='end')
+
+    start >> fact_sta_topup >> end
